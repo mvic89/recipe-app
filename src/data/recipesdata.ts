d60a3af95c87522f223b2a4ea48c76223a356410
Victor Furustubbe <mantykanto2@gmail.com>
2025-07-04 23:54:06 +0200

    added recipes content

--- src/data/recipesdata.ts
+++ src/data/recipesdata.ts
@@ -3,6 +3,8 @@ import type { Recipe } from "../utils/recipetypes";
 import gnocchiImg from "../assets/gnocchi.jpg";
 import pastinaImg from "../assets/pastina-soup.jpg";
 import trapaneseImg from "../assets/trapanese.jpg";
+import sorrentinaImg from "../assets/sorrentina.jpg";
+import vongoleImg from "../assets/vongole.jpg";
 
 export const Recipes: Recipe[] = [
     {
@@ -80,6 +82,57 @@ export const Recipes: Recipe[] = [
             {description: 'Add about 1/3 cup of the pasta water into the bowl, and then transfer in the pesto, along with a large pinch of salt, and toss to coat. More pasta water can be added to adjust the texture. Once mixed, add another optional handful of cheese, and toss one last time. Serve immediately.'}
         ],
         image: trapaneseImg
+    },
+    {
+        id: 4,
+        title: 'Gnocchi alla Sorrentina',
+        summary: `A classic Italian "primo", or starter pasta, from the Campania region of Italy. Fresh tomatoes are sauteed with onion, garlic, and basil, then tossed with fresh mozzarella, basil, and your favorite gnocchi or short pasta. In the summer, we skip the oven part of this recipe and just eat it after being tossed in the saute pan.`,
+        ingredients: [
+            { name: 'Olive oil', quantity:'2 teaspoons'},
+            { name: 'Potato gnocchi', quantity:'½ pound'},
+            { name: 'Small onion, chopped', quantity:'1'},
+            { name: 'Garlic, minced', quantity:'1 clove'},
+            { name: 'Italian plum tomatoes, halved', quantity:'14 ounces'},
+            { name: 'Dry white wine', quantity:'¼ cup'},
+            { name: 'Small fresh mozzarella balls (ciliegine)', quantity:'7 ounces'},
+            { name: 'Finely grated Parmigiano-Reggiano cheese', quantity:'4 tbsp'},
+            { name: 'Fresh basil', quantity:'4 sprigs'}
+
+        ],
+        steps: [
+            {description: 'Preheat the oven to 400 degrees F (200 degrees C). Lightly oil an oven-safe baking dish.'},
+            {description: 'Bring a large pot of lightly salted water to a boil. Cook gnocchi in the boiling water until they float to the top, 2 to 4 minutes. Drain and set aside.'},
+            {description: 'While gnocchi is cooking, heat olive oil in a skillet over medium heat. Add onion and cook until soft and translucent, about 5 minutes. Add garlic and cook until fragrant, about 30 seconds. Cut an "x" into the skin of each tomato with a sharp knife and add to the skillet; stir well. Pour in white wine and simmer until tomatoes are tender, about 10 minutes. Using a spoon, gently crush tomatoes to release juice and pulp. Add basil and continue to gently crush tomatoes to create a sauce. Remove and discard tomato skins as they loosen from the pulp. Cook for 1 more minute over low heat. Add drained gnocchi to the sauce and mix until covered with the sauce.'},
+            {description: `Remove skillet from heat and add mozzarella balls, folding everything together gently but quickly, so cheese is coated with sauce but not cooked or melted. Transfer to the prepared baking dish and sprinkle with Parmigiano-Reggiano cheese.`},
+            {description: 'Bake in the preheated oven until cheese has melted, about 10 minutes. Garnished with fresh basil sprigs. '}
+        ],
+        image: sorrentinaImg
+    },
+    {
+        id: 5,
+        title: 'Spaghetti Risottati alle Vongole',
+        summary: `Spaghetti is cooked like a risotto in fresh clam sauce for a flavor-packed dish. A few simple ingredients can create this delicious pasta dish that will wow you. The spaghetti take in the juices of the clams using minimal water. This is one of my family's favorite pasta dishes.`,
+        ingredients: [
+            { name: 'Water', quantity:'8 cups'},
+            { name: 'Salt', quantity:'1 tbsp'},
+            { name: 'Extra-virgin olive oil', quantity:'2 tbsp'},
+            { name: 'Red Thai chile pepper, seeded and chopped', quantity:'1'},
+            { name: 'Garlic, crushed', quantity:'1'},
+            { name: 'Verace clams in shell, scrubbed', quantity:'2 pounds'},
+            { name: 'White wine', quantity:'¾ cup'},
+            { name: 'Spaghetti', quantity:'12 ounces'},
+            { name: 'Lemon zest', quantity:'1 teaspoon'},
+            { name: 'Chopped flat-leaf (Italian) parsley', quantity:'½ cup'},
+            { name: 'Coarsely ground black pepper', quantity:'1 teaspoon'}
+        ],
+        steps: [
+            {description: 'Bring water to a boil in a stockpot; add salt. Reduce heat to a simmer. '},
+            {description: 'Heat a large skillet over medium heat. Add oil, followed by chile pepper and garlic. Cook and stir gently until fragrant, about 30 seconds. Remove garlic and chile pepper; discard. Add clams to the fragrant oil.'},
+            {description: 'Pour wine over the clams and cover immediately. Let simmer until clams have opened, about 7 minutes. Remove clams using a slotted spoon to a large bowl. Cover to avoid losing their steam. Discard any unopened clams.'},
+            {description: `Pour four ladles of salted water into the skillet with the juice of the clams. Bring to a boil over high heat. Add spaghetti; move around with a pasta fork to keep from sticking together. Cook, adding a ladle or two of salted water as needed to keep spaghetti barely covered, until pasta is tender yet firm to the bite, 8 to 10 minutes. Add clams and sprinkle in lemon zest. Give the skillet a hefty toss to gently mix the clams in the thickened sauce.`},
+            {description: 'Divide into four pasta bowls. Cover with parsley and serve immediately with empty plates on the side for the shells while eating.'}
+        ],
+        image: vongoleImg
     },
 
 ]
